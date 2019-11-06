41defe63d86f9bda0bd0e77c5ecacc22695765ae
Victor Yarema <victor.yarema@gmail.com>
2019-11-06 11:21:49 +0000

    Avoid triggering tab completion output in case of 2 consequtive tabs in bash.

--- Main.sh
+++ Main.sh
@@ -1,16 +1,16 @@
 ShEnvInstallerBootstrap()
 (
-	set -xuC &&
-	set -o pipefail &&
-	D="$1" &&
-	U="$2" &&
-	F='Main.sh' &&
-	mkdir -p "${D}" &&
-	cd "${D}" &&
-	{
-		[ -e "${F}" ] ||
-		wget -O "${F}" "${U}"
-	} &&
-	. ./"${F}"
+  set -xuC &&
+  set -o pipefail &&
+  D="$1" &&
+  U="$2" &&
+  F='Main.sh' &&
+  mkdir -p "${D}" &&
+  cd "${D}" &&
+  {
+    [ -e "${F}" ] ||
+    wget -O "${F}" "${U}"
+  } &&
+  . ./"${F}"
 ) &&
 ShEnvInstallerBootstrap "${HOME}/Tools/ShEnvInstaller/Raw" 'https://raw.githubusercontent.com/victor-yarema/ShEnvInstaller/master/Main.sh?md5=b4ad5d2c60b382f9baa234a020ae6329'
